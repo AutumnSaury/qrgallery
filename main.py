c005be99a6c8c5ec0c724421d70dc6c2dedbb18b
Autumn_Saury <autumnsaury@outlook.com>
2022-09-21 13:05:29 +0800

    ✨feature: properties are now imported from envs; added logging

--- main.py
+++ main.py
@@ -2,45 +2,66 @@ from watchdog.observers import Observer
 from LskyApis import LskyApis
 from NewPicsHandler import NewPicsHandler
 from MyQR import myqr
+from Silence import Silence
+from dotenv import load_dotenv
+import logging
 import os
+import re
 
-# 以下配置项请在打包前进行修改
+# 打包前请注释以下两行
+from dotenv import load_dotenv
+load_dotenv()
+
+# 日志输出设置
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s]: %(message)s'
+)
+
 # Lsky API Root
-API_ROOT = ""
+API_ROOT = os.getenv("API_ROOT")
 USER_INFO = {
     # 用户邮箱
-    "email": "",
+    "email": os.getenv("USREMAIL"),
     # 用户密码
-    "password": ""
+    "password": os.getenv("USERPWD")
 }
 # 背景图片路径
-BG_PIC = r""
+BG_PIC = os.getenv("BG_PIC")
 # 二维码存放路径
-QR_DIR = r""
+QR_DIR = os.getenv("QR_DIR")
 # 照片存放目录
-OBSERVED_DIR = r""
+OBSERVED_DIR = os.getenv("OBSERVED_DIR")
 
 lsky = LskyApis(API_ROOT, USER_INFO)
 
 
 def qr_gen(path):
     img_info = lsky.upload_img(path)
-    myqr.run(
-        words=img_info["links"]["url"],
-        version=5,
-        picture=BG_PIC,
-        colorized=True,
-        contrast=1.0,
-        brightness=1.0,
-        save_name=img_info["name"] + "qr.png",
-        save_dir=QR_DIR
-    )
+    img_file_name = re.search(r"(?<=\\)[^\\]+\w{3,4}$", path).group()
+    logging.info("已上传图片" + img_file_name +
+                 "，图片URL：" + img_info["links"]["url"])
+    logging.info("正在为图片" + img_file_name + "生成二维码")
+    # 关闭myqr成功运行后的line 16: mode: byte输出
+    with Silence():
+        myqr.run(
+            words=img_info["links"]["url"],
+            version=5,
+            picture=BG_PIC,
+            colorized=True,
+            contrast=1.0,
+            brightness=1.0,
+            save_name=img_info["name"] + "_qr.png",
+            save_dir=QR_DIR
+        )
+    logging.info("二维码已保存至" + img_info["name"] + "qr.png")
     try:
         os.startfile(QR_DIR + "\\" + img_info["name"] + "qr.png")
     except:
         raise Exception("尝试打开二维码文件时发生错误")
 
 
+# 创建并启动Observer线程
 ob = Observer()
 ob.schedule(NewPicsHandler(qr_gen), OBSERVED_DIR)
 ob.start()
